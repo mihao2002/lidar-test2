2b8bf8628756db1f9dab43c10280c30d0ff9c117
Hao Mi <mihao_2002@hotmail.com>
2025-06-22 09:04:19 -0700

    1

--- Lidar Scan/Modules/View 3D Scans/View3DScansView.swift
+++ Lidar Scan/Modules/View 3D Scans/View3DScansView.swift
@@ -86,7 +86,7 @@ struct View3DScansView: View {
         do {
             let fileURLs = try FileManager.default.contentsOfDirectory(at: folderURL, includingPropertiesForKeys: nil)
             let filteredURLs = fileURLs.filter { (url) -> Bool in
-                return url.pathExtension == "obj"
+                return url.pathExtension == "obj" || url.pathExtension == "usdz"
             }
             self.fileNames = filteredURLs.map { $0.lastPathComponent }
         } catch {
